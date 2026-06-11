b85c42414d1373445ff8a9c4adcfa424d20bb388
thebetteringbranch <thebetteringbranch@gmail.com>
2026-06-12 03:29:39 +0800

    Update point_redemption_confirmation.js

--- js/point_redemption_confirmation.js
+++ js/point_redemption_confirmation.js
@@ -2,29 +2,18 @@ if (sessionStorage.getItem("pointsRedeemed") === "true") {
     window.location.href = "stampcard.html";
 }
 
-sessionStorage.getItem("pointsRedeemed")
-
 console.log("Confirmation page script loaded");
 
-// Function to mark points as redeemed
 function redeemPoints() {
-    if (sessionStorage.getItem("pointsRedeemed") === "true") {
-        // Redirect to another page if redemption is already completed
-        window.location.href = "redeemed.html"; // Change to the appropriate page
-    }
     sessionStorage.setItem("pointsRedeemed", "true");
-    window.location.href = "point_redemption.html"; // Redirect after redemption
+    window.location.href = "point_redemption.html";
 }
 
-// Redirect to Redemption Page on "Yes"
 document.getElementById("yes-btn").addEventListener("click", () => {
     console.log("Yes button clicked");
-    // Replace the current history entry with the Redemption Page
-    history.replaceState(null, "", "point_redemption.html");
     redeemPoints();
 });
 
-// Redirect to Progress Page on "No, Go Back"
 document.getElementById("no-btn").addEventListener("click", () => {
     console.log("No button clicked");
     window.location.href = "stampcard.html";
